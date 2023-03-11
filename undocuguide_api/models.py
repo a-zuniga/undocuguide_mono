4c86727b9909f00e9d8447378c6e5da53570bce8
Alejandro Zuniga <azunigas@gmu.edu>
2023-03-11 13:44:14 -0500

    [api] updating post and put routes to have schema validations

--- undocuguide_api/models.py
+++ undocuguide_api/models.py
@@ -1,3 +1,15 @@
+scholarship_schema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "description": {"type": "string"},
+        "deadline": {"type": "string", "format": "data-time"},
+        "amount": {"type": "number"},
+        "url": {"type": "string"},
+    },
+    "required": ["name", "amount", "deadline"],
+}
+
 class Scholarship:
     def __init__(self, name, description, amount, deadline, url):
         self.name = name
--- undocuguide_api/scholarships.py
+++ undocuguide_api/scholarships.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, request, jsonify
 from models import Scholarship
 from db_operations import *
+from models import scholarship_schema
+from jsonschema import validate, SchemaError, FormatChecker
 
 scholarships = Blueprint('scholarships', __name__)
 
 @scholarships.route('/scholarships', methods=['GET'])
 def list_scholarships():
-    """Endpoint to list all scholarships in the database.
-
-    Returns:
-        json: A list of dictionaries representing the scholarships.
+    """
+    list all scholarships in the database.
     """
     scholarship_list = []
     for scholarship in scholarship_collection.find():
@@ -22,21 +22,14 @@ def list_scholarships():
 @scholarships.route('/scholarships', methods=['POST'])
 def create_scholarship():
     data = request.get_json()
-    if not data:
-        return jsonify({'message': 'No input data provided'}), 400
-
-    # Validate the input data using the schema
-    # errors = scholarship_schema.validate(data)
-    # if errors:
-    #     return jsonify(errors), 400
-
     try:
-        new_scholarship = Scholarship(name=data['name'], description=data['description'], amount=data['amount'], deadline=data['deadline'], url=data['url'])
-        create_scholarship_in_db(new_scholarship)
-        return jsonify({'message': 'Scholarship created successfully'})
-    except Exception as e:
-        print(data)
-        return jsonify({'message': 'Error creating scholarship', 'error': str(e)}), 500
+        validate(data, scholarship_schema, format_checker=FormatChecker())
+    except SchemaError as e:
+        return jsonify({'message': f'Schema validation error: {e}'}), 400
+
+    new_scholarship = Scholarship(name=data['name'], description=data['description'], amount=data['amount'], deadline=data['deadline'], url=data['url'])
+    create_scholarship_in_db(new_scholarship)
+    return jsonify({'message': 'Scholarship created successfully'})
     
 
 @scholarships.route('/scholarships/<scholarship_id>', methods=['GET'])
@@ -45,11 +38,17 @@ def read_scholarship(scholarship_id):
     if scholarship:
         return jsonify(scholarship)
     else:
-        return jsonify({'message': 'Scholarship not found'})
+        return jsonify({'message': 'Scholarship not found'}), 404
         
 
 @scholarships.route('/scholarships/<scholarship_id>', methods=['PUT'])
 def update_scholarship(scholarship_id):
+    data = request.get_json()
+    try:
+        validate(data, scholarship_schema, format_checker=FormatChecker())
+    except SchemaError as e:
+        return jsonify({'message': f'Schema validation error: {e}'}), 400
+
     scholarship = read_scholarship_from_db(scholarship_id)
     if scholarship:
         data = request.get_json()
